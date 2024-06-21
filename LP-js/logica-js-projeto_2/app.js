5d4604a8240c20910d82254a17508bba86e2d293
thnbi <renato_freitas13@icloud.com>
2024-06-21 17:04:03 -0300

    projeto 2 - verificando chute

--- LP-js/logica-js-projeto_2/app.js
+++ LP-js/logica-js-projeto_2/app.js
@@ -1,5 +1,26 @@
-const title = document.querySelector("h1");
-title.innerHTML = "Secret Number Game";
+function showText(tag, text) {
+	let field = document.querySelector(tag);
+	field.innerHTML = text;
+}
 
-const paragraph = document.querySelector("p");
-paragraph.innerHTML = "Chose a number between 1-10";
+showText("h1", "Secret Number Game");
+showText("p", "Chose a number between 1-10");
+
+function generateSecretNumber() {
+	let randomNumber = Math.floor(Math.random() * 10) + 1;
+	console.log("Secret number is: " + randomNumber);
+	return randomNumber;
+}
+
+let secretNumber = generateSecretNumber();
+
+function checkGuess() {
+	let guess = document.querySelector("input").value;
+	if (guess == secretNumber){
+		showText('h1', "Congratulations!");
+		showText(`p`, `The secret number was ${secretNumber}`);
+	}
+	else {
+		showText('h1', `Try again! The secret number was not ${guess}`);
+	}
+}
