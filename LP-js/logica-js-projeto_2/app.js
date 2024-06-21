e4ea7450e2a3e678ff002767be4820cd1bfa2b79
thnbi <renato_freitas13@icloud.com>
2024-06-21 16:17:51 -0300

    projeto LP 2 -  manipulando textos

---
+++ LP-js/logica-js-projeto_2/app.js
@@ -0,0 +1,5 @@
+const title = document.querySelector("h1");
+title.innerHTML = "Secret Number Game";
+
+const paragraph = document.querySelector("p");
+paragraph.innerHTML = "Chose a number between 1-10";
