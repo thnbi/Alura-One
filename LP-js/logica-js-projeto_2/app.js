8bc65b29dc6b50c2828deabf0f32db94b9bedf83
thnbi <renato_freitas13@icloud.com>
2024-06-21 22:51:49 -0300

    botao new game

--- LP-js/logica-js-projeto_2/app.js
+++ LP-js/logica-js-projeto_2/app.js
@@ -1,10 +1,12 @@
+function initialText() {
+	showText("h1", "Secret Number Game");
+	showText("p", "Chose a number between 1-10");
+}initialText();
+
 function showText(tag, text) {
 	let field = document.querySelector(tag);
 	field.innerHTML = text;
 }
-
-showText("h1", "Secret Number Game");
-showText("p", "Chose a number between 1-10");
 
 function generateSecretNumber() {
 	let randomNumber = Math.floor(Math.random() * 10) + 1;
@@ -12,15 +14,38 @@ function generateSecretNumber() {
 	return randomNumber;
 }
 
+function newGame() {
+	secretNumber = generateSecretNumber();
+	guesses = 1;
+	initialText();
+	document.getElementById("reiniciar").setAttribute("disabled", true);
+}
+
+function clearField() {
+	document.querySelector("input").value = "";
+}
+
+let guesses = 1;
 let secretNumber = generateSecretNumber();
 
 function checkGuess() {
 	let guess = document.querySelector("input").value;
 	if (guess == secretNumber){
 		showText('h1', "Congratulations!");
-		showText(`p`, `The secret number was ${secretNumber}`);
+
+		let guessWord = guesses == 1 ? "guess" : "guesses";
+		let message = `You guessed the secret number in ${guesses} ${guessWord}!`;
+		showText(`p`, `${message}`);
+		document.getElementById("reiniciar").removeAttribute("disabled")
 	}
 	else {
-		showText('h1', `Try again! The secret number was not ${guess}`);
+		if(guess > secretNumber) {
+			showText('p', "Try again! The secret number is lower");
+		}
+		else {
+			showText('p', "Try again! The secret number is higher");
+		}
+		guesses++;
 	}
+	clearField();
 }
